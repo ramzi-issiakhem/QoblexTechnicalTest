edcfeeb00c224205197a96a71f2c5a60a4e6a0b5
Issiakhem Mohamed Ramzi <issiakhem.mohamedramzi@gmail.com>
2024-06-23 22:06:00 +0100

    Init

--- BundleProcessing/BundleProcessing/obj/Debug/net8.0/BundleProcessing.AssemblyInfo.cs
+++ BundleProcessing/BundleProcessing/obj/Debug/net8.0/BundleProcessing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BundleProcessing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0344d3240ed9c13676c3323f4e132d5a8919cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BundleProcessing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BundleProcessing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
